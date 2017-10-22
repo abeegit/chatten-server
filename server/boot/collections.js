a28d2bcb013708f3af16585bb2e15d9ebd644c41
Abhijeet Kumar <abhijeetkumar_1992@yahoo.in>
2017-10-22 14:18:26 +0530

    Initial commit

---
+++ server/boot/collections.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const person = require("../models/person.json");
+const colors = require("colors");
+
+module.exports = function(server) {
+  let ds = server.dataSources.mongo;
+  ds.autoupdate(person.name, function(err, result) {
+    if (err) {
+      console.log(
+        `Error autoupdating collection ${person.name} ${err.toSting()} `.red
+      );
+    } else {
+      console.log(`Collections ${person.name} ready`.dim);
+    }
+  });
+};
---
+++ server/models/person.js
@@ -0,0 +1,5 @@
+"use strict";
+
+const schema = require("./person.json");
+
+module.exports = function(Person) {};
