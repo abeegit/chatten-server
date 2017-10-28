c435688e76f798bbcdb82d6614bcd34261251e0f
Abhijeet Kumar <abhijeetkumar_1992@yahoo.in>
2017-10-29 04:06:57 +0530

    Implementing JWT WIP

--- server/models/person.js
+++ server/models/person.js
@@ -2,8 +2,12 @@
 
 const schema = require("./person.json");
 const utility = require("./utility");
+const jwt = require("jsonwebtoken");
 
 module.exports = function(Person) {
+  Person.validatesUniquenessOf("email", {
+    message: "You have already registered"
+  });
   Person.on("dataSourceAttached", () => {
     const create = Person.create;
 
@@ -26,5 +30,50 @@ module.exports = function(Person) {
       };
       create.call(Person, person, customCb);
     };
+  });
+
+  Person.login = (email, password, cb) => {
+    let cert = fs.readFileSync("./../keys/jwt.key");
+    console.log(cert);
+    Person.findOne(
+      { fields: ["salt", "password"], where: { email: "afasfs" } },
+      (err, person) => {
+        if (err) {
+          cb(err);
+        }
+        if (
+          person &&
+          utility.getSaltHashPassword(password, person.salt) === person.password
+        ) {
+          let token = jwt.sign({}, cert, { algorithm: "RS256" });
+          cb(null, { status: "success", accessToken: "fskhasfks" });
+        } else {
+          cb({ statusCode: "400", status: "failure" });
+        }
+      }
+    );
+  };
+
+  Person.remoteMethod("login", {
+    accepts: [
+      {
+        arg: "email",
+        description: "User email",
+        required: true,
+        type: "string"
+      },
+      {
+        arg: "password",
+        description: "User password",
+        required: true,
+        type: "string"
+      }
+    ],
+    description: "Method to login the user using his/her email and password",
+    http: { path: "/login", verb: "post", status: 200, errorStatus: 400 },
+    returns: [
+      { arg: "status", type: "string" },
+      { arg: "accessToken", type: "string" }
+    ]
   });
 };
